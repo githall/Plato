10ee1c5f27ad0de184b23bbd4f489a5bbf07e485
InstantASP <sales@instantasp.co.uk>
2019-11-28 22:38:37 +0000

    FEAT: Adding theme selection via cookie support for Plato.UWP

--- src/Plato.Internal.Theming/ThemeSelector.cs
+++ src/Plato.Internal.Theming/ThemeSelector.cs
@@ -10,6 +10,7 @@ namespace Plato.Internal.Theming
     {
 
         private const string HeaderName = "X-Plato-Theme";
+        private const string CookieName = "plato-theme";
 
         private const string LightThemeFolder = "/default";
         private const string DarkThemeFolder = "/dark";
@@ -31,30 +32,69 @@ namespace Plato.Internal.Theming
         public string GetThemePath()
         {
 
-            // Search headers for a valid default theme
-            var theme = GetThemeSetViaRequestHeader();
+            // Theme can also be set via request headers or cookies
+            var theme = GetHttpTheme();
             if (!string.IsNullOrEmpty(theme))
             {
                 return _themeOptions.VirtualPathToThemesFolder + theme;
             }
 
-            // Site theme
+            // Use site's configured theme
             if (!string.IsNullOrEmpty(_siteOptions.Theme))
             {
                 return _siteOptions.Theme;
             }
 
-            // Default theme
+            // Use default theme
             return _themeOptions.VirtualPathToThemesFolder + LightThemeFolder;
 
         }
         
-        string GetThemeSetViaRequestHeader()
+
+        string GetHttpTheme()
+        {
+
+            var headerTheme = GetViaHttptHeaders();
+            if (!string.IsNullOrEmpty(headerTheme))
+            {
+                return headerTheme;
+            }
+
+            var cookieTheme = GetViaHttpCookies();
+            if (!string.IsNullOrEmpty(cookieTheme))
+            {
+                return cookieTheme;
+            }
+
+            return string.Empty;
+
+        }
+
+        string GetViaHttptHeaders()
         {
 
             if (_httpContext.Request.Headers.ContainsKey(HeaderName))
             {
                 var themeName = _httpContext.Request.Headers[HeaderName].ToString();
+                switch (themeName.ToLowerInvariant())
+                {
+                    case "light":
+                        return LightThemeFolder;
+                    case "dark":
+                        return DarkThemeFolder;
+                }
+            }
+
+            return string.Empty;
+
+        }
+
+        string GetViaHttpCookies()
+        {
+
+            if (_httpContext.Request.Cookies.ContainsKey(CookieName))
+            {
+                var themeName = _httpContext.Request.Cookies[CookieName].ToString();
                 switch (themeName.ToLowerInvariant())
                 {
                     case "light":
